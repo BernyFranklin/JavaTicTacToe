d7b384afc867df02b131465e74349de0d20b1ceb
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-22 15:10:00 -0700

    Shell created

--- src/javatictactoe/JavaTicTacToe.java
+++ src/javatictactoe/JavaTicTacToe.java
@@ -6,19 +6,13 @@
  * Output: Game display
  * 22 Mar 2022
  */
+
 package javatictactoe;
-
-/**
- *
- * @author frankbernal
- */
+//
 public class JavaTicTacToe {
-
-    /**
-     * @param args the command line arguments
-     */
+    //
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+    }   // End of PSV Main
     
-}
+}   // End of JavaTicTacToe
