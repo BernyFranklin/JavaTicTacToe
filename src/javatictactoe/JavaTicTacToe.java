5d4196f6c357648b5aa7fd4f884766f11b9da262
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-23 12:18:01 -0700

    global array[][] board created with values 1-9

--- src/javatictactoe/JavaTicTacToe.java
+++ src/javatictactoe/JavaTicTacToe.java
@@ -8,9 +8,16 @@
  */
 
 package javatictactoe;
-//
+// Import the scanner
+import java.util.Scanner;
+
 public class JavaTicTacToe {
-    //
+    // Declare array for board with 3 rows and 3 cols
+    static char[ ][ ] board = {   // Index for positions in array
+        {'1', '2', '3'},          // [0][0] [0][1] [0][2]
+        {'4', '5', '6'},          // [1][0] [1][1] [1][2]
+        {'7', '8', '9'}           // [2][0] [2][1] [2][2]
+    };
     public static void main(String[] args) {
         // TODO code application logic here
     }   // End of PSV Main
