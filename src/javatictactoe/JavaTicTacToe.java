d543023ceb4780e99d026f48906e94f8400f2dc5
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-03-24 10:46:17 -0700

    Added finishing comments and some formatting spaces.

--- src/javatictactoe/JavaTicTacToe.java
+++ src/javatictactoe/JavaTicTacToe.java
@@ -10,6 +10,7 @@
 package javatictactoe;
 // Import the scanner
 import java.util.Scanner;
+// Import math functions
 import java.lang.Math;
 
 public class JavaTicTacToe {
@@ -20,18 +21,20 @@ public class JavaTicTacToe {
     // Global Variables
     static int squareCounter = 0;   // Keep track # of squares used
     static char player;             // Current player set at each turn
+    
     // Declare array for board with 3 rows and 3 cols
     static char[ ][ ] board = {   // Index for positions in array
         {'1', '2', '3'},          // [0][0] [0][1] [0][2]
         {'4', '5', '6'},          // [1][0] [1][1] [1][2]
         {'7', '8', '9'}           // [2][0] [2][1] [2][2]
     };
+    
+    // Start main
     public static void main(String[] args) {
         // Local variables for main
         char gameWinner = '-';   // Winner of hame X or O
         char inputCharacter;     // Input from user (should be 1-9)
         int choice;              // Input converted to index 0-8
-        char playAgain;
         
         // Create scanner object
         Scanner stdin = new Scanner(System.in);
@@ -55,8 +58,10 @@ public class JavaTicTacToe {
         // Start loop for gameplay
         while (squareCounter < 9 && gameWinner != 'X' && gameWinner != 'O') {
             // Loop goes until squares run out or a winner is determined
+            
             // Divider
             System.out.println ("=================================");
+            
             // Call board
             displayTicTacToe(board);
 
@@ -69,8 +74,10 @@ public class JavaTicTacToe {
             // Convert inputCharacter from ascii (1-9) to int (0-8)
             // To select the row and column index for the 3 by 3 array
             choice = inputCharacter - '1';   // Input is now 0-8
+            
             // Row will be [0] for (0 1 2) [1] for (3 4 5) [2] for (6 7 8)
             int row = choice / ROW_TOTAL;
+            
             // Col will be [0] for (0 3 6) [1] (1 4 7) [2] for (2 5 8)
             int col = choice % COL_TOTAL;
 
@@ -81,10 +88,13 @@ public class JavaTicTacToe {
 
             // Check if there is a winner
             // Returns X or O if game is won
+            // Returns Tie Game if no winner
             gameWinner = checkForWinningGame(board);
         }   // End of loop
 
         // The game is over. Either game was been one or we ran out of squares
+        // Print divider
+        System.out.printf ("=================================\n");
         // Display final board
         displayTicTacToe(board);
         // Blank line
@@ -99,6 +109,7 @@ public class JavaTicTacToe {
 
         // Close the scanner
         stdin.close();
+        
     }   // End of PSV Main
     
     // Start whoGoesFirst
@@ -117,11 +128,14 @@ public class JavaTicTacToe {
         // Blank line
         System.out.println();
         // Print vertical lines above 
-        System.out.println("     |     |     ");
+        System.out.println("             |     |             ");
         
         // Outer loop for rows
         for (int row = 0; row < 3; row++) {
             // Inner loop for cols
+            // Print 8 spaces for looks
+            System.out.printf("        ");
+            
             for (int col = 0; col <3; col++) {
                 // First 2 cols get a vertical line
                 if (col < 2)
@@ -130,15 +144,18 @@ public class JavaTicTacToe {
                 else
                     System.out.printf ("  %c  \n", board[row][col]);
             }   // End of col inner loop
+            
             // First two rows get a bottom line
             if (row <2)
-                System.out.println ("-----|-----|-----");
-            // Last one does not
+                System.out.println ("        -----|-----|-----");
+            // Last row does not
             else
-                System.out.println ("     |     |     ");
+                System.out.println ("             |     |     ");
         }   // End of row outer loop
+        
         // Blank line for formatting
         System.out.println();
+        
     }   // End of displayTicTacToe
     
     // Start of runInput
@@ -146,16 +163,20 @@ public class JavaTicTacToe {
         // Check if input is between 0-8
         if (choice < 0 || choice > 8)
             System.out.printf ("Illegal value, please try again\n\n");
+        
         // Check if square is already ocupied
         else if (board[row][col] == 'X' || board[row][col] == 'O')
+            // Print the space is occupied
             System.out.printf ("This space has already been used, "
                     + "please try again\n\n");
+        
         // Enter square input as player selection
         else {
             // Enter X or O depending on who's turn it is
             board[row][col] = player;
             // Update counter
             squareCounter++;
+            
             // Select next player
             if (player == 'X')
                 // switch to O if X
@@ -164,49 +185,58 @@ public class JavaTicTacToe {
                 // switch to X if O
                 return 'X';
         }   // End of Else
+        
+        // If space already in use or bad input
+        // Original player returned to try again
         return player;
+        
     }   // End of runInput
     
     // Start of checkForWinningGame
     public static char checkForWinningGame(char[][] board) {
         // Check for all 8 possibilities for a winner
-        //   012  012  012  012  012  012  012  012
-        // 0 XXX  ---  ---  X--  -X-  --X  X--  --X
-        // 1 ---  XXX  ---  X--  -X-  --X  -X-  -X-
-        // 2 ---  ---  XXX  X--  -X-  --X  --X  X--
-        //
+        //     [0][1][2]  [0][1][2]  [0][1][2]  [0][1][2]
+        // [0]  X  X  X    *  *  *    *  *  *    X  *  * 
+        // [1]  *  *  *    X  X  X    *  *  *    X  *  * 
+        // [2]  *  *  *    *  *  *    X  X  X    X  *  *  
+        //    ============================================
+        // [0]  *  X  *    *  *  X    X  *  *    *  *  X
+        // [1]  *  X  *    *  *  X    *  X  *    *  X  *
+        // [2]  *  X  *    *  *  X    *  *  X    X  *  *
+        
         // Who owns the square
         char player;
         
-        // Top Row, Left Col, of Left Diagonal
+        // Check for winners
+        // Top Row, Left Col, or Left Diagonal
         // Starting at [0][0]
         player = board[0][0];
-        if ((board[0][1] == player && board[0][2] == player) ||      // Top Row
-                (board[1][0] == player && board[2][0] == player) ||  // L.Col
-                (board[1][1] == player && board[2][2] == player))    // L.Diag
+        if ((board[0][1] == player && board[0][2] == player) ||     //1 Top Row
+                (board[1][0] == player && board[2][0] == player) || //2 L.Col
+                (board[1][1] == player && board[2][2] == player))   //3 L.Diag
             return player;
         
         // Middle Col
         // Starting at [0][1]
         player = board[0][1];
-        if (board[1][1] == player && board[2][1] == player)
+        if (board[1][1] == player && board[2][1] == player)         //4 M. Col
             return player;
         
         // Right Col and Left Diagonal
         // Starting at [0][2]
         player = board[0][2];
-        if ((board[1][2] == player && board[2][2] == player) ||    // R. Col
-                (board[1][1] == player && board[2][0] == player))  // R.Diag
+        if ((board[1][2] == player && board[2][2] == player) ||     //5 R. Col
+                (board[1][1] == player && board[2][0] == player))   //6 R.Diag
             return player;
         
         // Middle row starting at [1][0]
         player = board[1][0];
-        if (board[1][1] == player && board[1][2] == player)
+        if (board[1][1] == player && board[1][2] == player)         //7 M. Row
             return player;
         
         // Bottom row starting at [2][0]
         player = board[2][0];
-        if (board[2][1] == player && board[2][2] == player)
+        if (board[2][1] == player && board[2][2] == player)         //8 B. Row
             return player;
         
         // No Winner
